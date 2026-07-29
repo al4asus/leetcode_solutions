0b715f8b27944dfad0f07bcc56a4b803b7a45776
Hasan Geredeli <105155448+al4asus@users.noreply.github.com>
2026-07-29 18:47:55 +0000

    Sync LeetCode submission Runtime - 71 ms (52.36%), Memory - 19.7 MB (73.87%)

---
+++ solutions/0076-minimum-window-substring/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t or len(s) < len(t):
+            return ""
+
+        t_count = Counter(t)
+
+        required = len(t_count)
+        formed = 0
+        window_counts = {}
+        left, right = 0,0
+
+        ans = float("inf"), None, None
+
+        while right < len(s):
+            char = s[right]
+            window_counts[char] = window_counts.get(char, 0) + 1
+
+            if char in t_count and window_counts[char] == t_count[char]: formed += 1
+
+            while left <= right and formed == required:
+                char_left = s[left]
+
+                if right - left + 1 < ans[0]:
+                    ans = (right - left +1, left, right)
+            
+                window_counts[char_left] -= 1
+
+                if char_left in t_count and window_counts[char_left] < t_count[char_left]: formed -= 1
+        
+
+                left += 1
+    
+            right += 1
+
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
